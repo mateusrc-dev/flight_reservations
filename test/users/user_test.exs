ad7efc9f43c7b2bd7742621d4ad0371e134d8580
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-07-06 10:39:05 -0300

    created user test

--- test/users/user_test.exs
+++ test/users/user_test.exs
@@ -4,5 +4,18 @@ defmodule FlightReservations.Users.UserTest do
   alias FlightReservations.Users.User
 
   describe "build/1" do
+    test "when all the params are valid, returns the user" do
+      response = User.build("Mateus", "mateus@bananas.com", "123456")
+
+      assert {:ok, %User{id: _id, name: _name, email: _email, cpf: _cpf}} = response
+    end
+
+    test "when there are invalid params, returns an error" do
+      response = User.build("Mateus", "mateus@bananas.com", 123_456)
+
+      expected_response = {:error, 'Invalid parameters.'}
+
+      assert expected_response == response
+    end
   end
 end
